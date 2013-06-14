a2e706ae0079f7ce26687d3b9ddfb490bdb4ef6c
cutsea110 <cutsea110@gmail.com>
2013-06-14 18:44:07 +0900

    FIX: could not use correctly by yesod plugins

--- Yesod/Goodies/PNotify.hs
+++ Yesod/Goodies/PNotify.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, QuasiQuotes, TemplateHaskell, MultiParamTypeClasses, TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, TemplateHaskell, MultiParamTypeClasses, TypeFamilies, FlexibleContexts #-}
 module Yesod.Goodies.PNotify 
        ( PNotify(..)
        , NotifyType(..)
@@ -17,6 +17,7 @@ import Data.Text (Text)
 import Data.Monoid ((<>), mempty)
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
+import Control.Monad.Trans.Resource
 import Control.Monad.Trans.Maybe
 import Data.Char (toLower)
 import Text.Julius (RawJS(..))
@@ -46,21 +47,39 @@ class YesodJquery a => YesodJqueryPnotify a where
 notifyKey :: Text
 notifyKey = "_PNotify"
 
-_setPNotify :: [PNotify] -> HandlerT site IO ()
+_setPNotify :: (MonadThrow m, 
+                MonadUnsafeIO m,
+                MonadBaseControl IO m,
+                MonadIO m) => 
+               [PNotify] -> HandlerT site m ()
 _setPNotify = setSession notifyKey . T.concat . TL.toChunks . TL.pack . show
 
-getPNotify :: HandlerT site IO (Maybe [PNotify])
+getPNotify :: (MonadThrow m, 
+               MonadUnsafeIO m,
+               MonadBaseControl IO m,
+               MonadIO m) => 
+              HandlerT site m (Maybe [PNotify])
 getPNotify = runMaybeT $ do
   ns <- MaybeT $ lookupSession notifyKey
   lift $ deleteSession notifyKey
   return $ read $ T.unpack ns
 
-setPNotify :: PNotify -> HandlerT site IO ()
+setPNotify :: (Monad m,
+               MonadThrow m,
+               MonadUnsafeIO m,
+               MonadBaseControl IO m,
+               MonadIO m) =>
+              PNotify -> HandlerT site m ()
 setPNotify n = do
   mns <- getPNotify
   _setPNotify (n:maybe [] id mns)
 
-pnotify :: YesodJqueryPnotify site => site -> WidgetT site IO ()
+pnotify :: (YesodJqueryPnotify site, 
+            MonadThrow m, 
+            MonadUnsafeIO m, 
+            MonadBaseControl IO m,
+            MonadIO m) => 
+           site -> WidgetT site m ()
 pnotify y = do
   mnotify <- handlerToWidget getPNotify
   case mnotify of
