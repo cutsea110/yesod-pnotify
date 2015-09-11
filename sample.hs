9336c78cc24794c9da0f6adc36d161ce2818ed9d
cutsea110 <cutsea110@gmail.com>
2015-09-11 12:09:01 +0900

    like bootstrap3

--- sample.hs
+++ sample.hs
@@ -5,6 +5,7 @@ import qualified Data.Text as T
 
 import Yesod
 import Yesod.Form.Jquery
+import Yesod.Form.Bootstrap3
 import Yesod.Goodies.PNotify
 
 data Demo = Demo
@@ -18,6 +19,8 @@ instance Yesod Demo where
   defaultLayout widget = do
     y <- getYesod
     pc <- widgetToPageContent $ do
+      addScriptEither $ urlBootstrap3Js y
+      addStylesheetEither $ urlBootstrap3Css y
       widget
       pnotify y
     withUrlRenderer [hamlet|
@@ -39,53 +42,58 @@ instance RenderMessage Demo FormMessage where
 
 
 data Account = Account { ident :: Text, passwd :: Text } deriving Show
-accountForm :: Html -> MForm Handler (FormResult Account, Widget)
-accountForm = renderDivs $ Account
-              <$> areq textField "Id" Nothing
-              <*> areq passwordField "Pass" Nothing
+accountForm :: AForm Handler Account
+accountForm = Account
+              <$> areq textField (bfs ("Id" :: Text)) Nothing
+              <*> areq passwordField (bfs ("Pass" :: Text)) Nothing
 
 getLoginR :: Handler Html
 getLoginR = do
-  (w, e) <- generateFormPost accountForm
+  (w, e) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm accountForm
   defaultLayout $ do
     setTitle "Login"
     [whamlet|<p>Login
-     <form method=post action=@{LoginR} enctype=#{e}>
+     <form role=form method=post action=@{LoginR} enctype=#{e}>
        ^{w}
-       <input type=submit value=Login>
+       <button type=submit .btn .btn-primary>Sign in
      <span>Please input guest/guest for ID/Pass, and Click Login button.
     |]
 
 postLoginR :: Handler Html
 postLoginR = do
-  ((r, w), e) <- runFormPost accountForm
+  ((r, w), e) <- runFormPost $ renderBootstrap3 BootstrapBasicForm accountForm
   case r of
     FormSuccess acc ->
       if ident acc == passwd acc
       then do
         setPNotify $ defaultPNotify { _type = Just Success
-                                    , _styling = Just BrightTheme
+                                    , _styling = Just Bootstrap3
                                     , _title = Just $ Right "Hello"
                                     , _text = Just $ Right $ "Welcome, " `T.append` ident acc
                                     }
         redirect (HomeR $ ident acc)
       else do
         setPNotify $ defaultPNotify { _type = Just Error
-                                    , _styling = Just BrightTheme
+                                    , _styling = Just Bootstrap3
                                     , _title = Just $ Right "Try again"
                                     , _text = Just $ Right "Please match the Id and the Pass."
                                     }
         redirect LoginR
     _ -> do
       setPNotify $ defaultPNotify { _type = Just Error
-                                  , _styling = Just BrightTheme
+                                  , _styling = Just Bootstrap3
                                   , _title = Just $ Right "Fail"
                                   , _text = Just $ Right "What happen?"
                                   }
       redirect LoginR
 
 getHomeR :: Text -> Handler Html
-getHomeR name = defaultLayout [whamlet|<p>`#{name}' Logged in.|]
+getHomeR name = defaultLayout $ do
+  setTitle "Home"
+  [whamlet|
+   <p>`#{name}' Logged in.
+   <a href=@{LoginR} .btn .btn-primary>Sign out
+  |]
 
 main :: IO ()
 main = warp 3000 Demo
